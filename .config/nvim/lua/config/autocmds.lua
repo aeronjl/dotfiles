1842c44282f70e33aeb5db07186c77b56a7de752
Aeron <ajlaffere@gmail.com>
2024-06-25 17:51:30 +0100

    remove autocmd (not useful)

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,7 +1,2 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
-
--- Automatically open new splits to the right
-vim.cmd([[
-  autocmd WinNew * wincmd L
-]])
